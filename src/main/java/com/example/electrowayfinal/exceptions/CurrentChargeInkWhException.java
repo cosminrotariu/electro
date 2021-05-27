74b1f6e74e102bac0181c3aa86af7211b8746686
Curca Andrei <curcaandrei99@gmail.com>
2021-05-28 01:06:00 +0300

    REMOVAL

--- src/main/java/com/example/electrowayfinal/exceptions/CurrentChargeInkWhException.java
+++
@@ -1,7 +0,0 @@
-package com.example.electrowayfinal.exceptions;
-
-public class CurrentChargeInkWhException extends Exception{
-    public CurrentChargeInkWhException(Double currentCharge, Double batteryCapacity) {
-        super(currentCharge + " is not less than " + batteryCapacity + ". It should be.");
-    }
-}
