ac19ecff12dc72835702d4f28237051f115951f6
Curca Andrei <curcaandrei99@gmail.com>
2021-05-27 21:09:14 +0300

    removal

--- src/main/java/com/example/electrowayfinal/exceptions/CarNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.electrowayfinal.exceptions;
-
-public class CarNotFoundException extends Exception {
-    public CarNotFoundException(Long id) {
-        super("Car with id " + id + " not found.");
-    }
-}
--- src/main/java/com/example/electrowayfinal/exceptions/ImpossibleRouteException.java
+++
@@ -1,7 +0,0 @@
-package com.example.electrowayfinal.exceptions;
-
-public class ImpossibleRouteException extends Exception{
-    public ImpossibleRouteException() {
-        super("There's no way we can get from A to B.");
-    }
-}
